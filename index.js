ddbb8d50a2ac948b81df720da356263be8eb4aeb
jjcourtney <jcourtneywork@gmail.com>
2023-02-20 21:25:02 +0000

    adds vercel json for deployment

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Get request to the homepage')
+    res.send('Get request to the homepage');
 })
 app.get('/test', (req, res) => {
-    res.send('Get request to the test page')
+    res.send('Get request to the test page');
 })
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`);
 })
+
+export default app;
