378f52466300e0e1004144dbbb4cdf0f7980dc8e
jjcourtney <jcourtneywork@gmail.com>
2023-02-20 21:20:06 +0000

    changes test load message

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const port = 3000
 app.get('/', (req, res) => {
     res.send('Get request to the homepage')
 })
-
+app.get('/test', (req, res) => {
+    res.send('Get request to the test page')
+})
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
