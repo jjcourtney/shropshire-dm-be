93024c67c50577a815a9d7c9bfff44fb876673aa
jjcourtney <jcourtneywork@gmail.com>
2023-02-22 17:32:52 +0000

    vercel issues - adds middleware

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,13 @@ import magicItemsRoute from './magicItems.js';
 
 const app = express()
 
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.use('/vehicles', vehiclesRoute);
 app.use('/magic-items', magicItemsRoute);
 
